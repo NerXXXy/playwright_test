b4a8e656d94bbf6b54906e275901908abd7b3d7a
NerXXXy <nerxxxxy@gmail.com>
2022-06-17 19:17:47 +0300

    practice for 03

--- 03_bonus.js
+++ 03_bonus.js
@@ -2,6 +2,7 @@ const {chromium} = require('playwright');
 
 (async () => {
 
+
     const browser = await chromium.launch({headless:false, slowMo:50})
     const context = await browser.newContext()
     const page = await context.newPage()
